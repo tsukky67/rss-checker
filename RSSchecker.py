e70ab55e76acd6cae5307c61bf896ca9f18cd34d
tsukky67 <tsukky67@keio.jp>
2022-01-07 04:33:37 +0900

    Update RSSchecker.py

--- RSSchecker.py
+++ RSSchecker.py
@@ -1,4 +1,5 @@
 import discord
+import feedparser
 import nest_asyncio
 nest_asyncio.apply()
 
@@ -7,9 +8,24 @@ client = discord.Client()
 
 #bot起動完了時に実行される処理
 
+CHANNEL_ID = 927990496813539361
+
+async def message():
+    url = 'https://maywork.net/feed/'  # rssのアドレス
+    feed = feedparser.parse(url)
+    title = feed.entries[0].title
+    link = feed.entries[0].link
+
+    channel = client.get_channel(CHANNEL_ID)
+    
+    await channel.send(title+' '+link)
+
+
 @client.event
 async def on_ready():
     print('ログイン成功')
+    await message()
+    
 
 #メッセージ受信時に実行される処理
 
