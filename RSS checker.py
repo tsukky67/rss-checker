d6f5f3ce687d1282f0a6ba1e3de6415735816925
tsukky67 <tsukky67@keio.jp>
2022-01-07 03:57:59 +0900

    Update RSS checker.py

--- RSS checker.py
+++ RSS checker.py
@@ -6,17 +6,10 @@ TOKEN = "OTI3OTkzMzIzNDM0Njk2NzI0.YdSTIQ.BN72FbC3_SaYMkiGVH0Jr5e-9pY"
 client = discord.Client()
 
 #bot起動完了時に実行される処理
-CHANNEL_ID = 927990496813539361
-
-
-async def greet():
-    channel = client.get_channel(CHANNEL_ID)
-    await channel.send('ログインしました')
-
 
 @client.event
 async def on_ready():
-    await greet()
+    print('ログイン成功')
 
 #メッセージ受信時に実行される処理
 
