f87a80e1c4f3291606aa3768017c7aaf1a14777a
tsukky67 <tsukky67@keio.jp>
2022-01-12 10:35:46 +0900

    Update RSSchecker.py

--- RSSchecker.py
+++ RSSchecker.py
@@ -75,49 +75,6 @@ async def check():
 
                 await channel.send(title+' '+link)
 
-
-async def nowcheck():
-    if __name__ == '__main__':
-        con = connect()
-
-        sql = 'select distinct url,latest_link from pages'
-
-        res = select_execute(con, sql)
-
-        for r in res:
-            feed = feedparser.parse(r[0])
-            title = feed.entries[0].title
-            link = feed.entries[0].link
-
-            def updatet_execute(con, slq):
-                with con.cursor() as cur:
-                    cur.execute(sql, (link, r[0]))
-
-                con.commit()
-            sql = """select * from pages where url = '""" + r[0] + """'"""
-            if select_execute(con, sql) != link:
-
-              sql = """update pages set latest_link = %s WHERE url = %s"""
-
-              updatet_execute(con, sql)
-
-              sql = """select distinct channel_id,url from users where url = '""" + r[0] + """'"""
-
-              res1 = select_execute(con, sql)
-
-              if res1 == []:
-                if __name__ == '__main__':
-                  con = connect()
-                  sql = """delete
-                              from pages
-                                where url = '""" + r[0] + "'"
-
-              else:
-                channel = client.get_channel(int(res1[0][0]))
-
-                await channel.send(title+' '+link)
-
-
 @client.event
 async def on_ready():
     print('ログイン成功')
@@ -160,7 +117,7 @@ async def on_message(message):
                 await message.channel.send("多分できてるよ")
 
     if message.content.startswith('/checkrss'):
-        sql = """select distinct channel_id,url from users where channel_id = '""" + message.channel.id + """'"""
+        sql = """select distinct channel_id,url from users where channel_id = '""" + str(message.channel.id) + """'"""
         res = select_execute(con, sql)
         for r in res:
             feed = feedparser.parse(r[1])
